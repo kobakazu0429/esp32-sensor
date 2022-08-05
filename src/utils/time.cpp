0e80282137738cf516f7b339fe2eae151d586d68
kobakazu0429 <kobakazu10@gmail.com>
2022-08-06 00:32:53 +0900

    fix: time

--- src/utils/time.cpp
+++ src/utils/time.cpp
@@ -1,20 +1,15 @@
 #include "time.hpp"
 
-#include <Arduino.h>
-#include <esp_sntp.h>
-#include <time.h>
+bool initializeTime() {
+  if (!isWiFiConnected()) {
+    return false;
+  }
 
-void initializeTime() {
   // API仕様上NTPサーバは3個まで指定できるようですが、実際には最初の1個だけが有効だと思われます。
   // https://garretlab.web.fc2.com/arduino/esp32/reference/cores/time/configTime.html
   configTime(JST, SUMMER_TIME, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
 
-  // Serial.print("[NTP Svr] Connecting.");
-
-  // while (isInitializedTime()) {
-  //   Serial.print(".");
-  //   delay(1000);  // １秒毎にリトライ
-  // }
+  return true;
 }
 
 bool isReadyTime() {
--- src/utils/time.hpp
+++ src/utils/time.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <esp_sntp.h>
+#include <time.h>
+
+#include "../wifi/wifi.hpp"
+
 #define JST 3600 * 9
 #define SUMMER_TIME 0
 
-void initializeTime();
+bool initializeTime();
 bool isReadyTime();
 unsigned long getUnixTimeAsSeconds();
